67f2f23315b164362875889941f7c1041185b08d
Jose Lobato <joselobatof@gmail.com>
2021-10-10 20:08:17 -0500

    add bootstrap vue, axios and numeral

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+// Paquetes de NPM
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Archivos locales de la app
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Traer el css global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// configuración extra
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+// isntancia principal
 new Vue({
   router,
   store,
