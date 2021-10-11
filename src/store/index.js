e8f243d7cf01d82b883550d490713aae664c0f10
Jose Lobato <joselobatof@gmail.com>
2021-10-10 22:53:09 -0500

    add modules to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// importamos modulos
+import oauth from './modules/oauth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    oauth
   }
 })
