8bd1cead2a9bfe7e7f85501fe56ccd657f38f010
Jose Lobato <joselobatof@gmail.com>
2021-10-11 00:16:30 -0500

    add footer, header, font awesome

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="container">
+    <Header/>
+
     <router-view/>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header/Index'
+import Footer from '@/components/Footer/Index'
 
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Iconos de tipo "Solid"
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+// Iconos de tipo "Brand" (marcas o logos de empresas)
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+// El componente que vamos a utilizar
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 // Archivos locales de la app
 import App from './App.vue'
 import router from './router'
@@ -13,9 +22,25 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 // CSS global
 import './assets/css/main.styl'
 
+// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
 // configuración extra
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // isntancia principal
 new Vue({
