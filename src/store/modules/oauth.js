3d131eba00502b8d06ee35fd9a6773e4a93491a1
Jose Lobato <joselobatof@gmail.com>
2021-10-10 22:53:30 -0500

    add module to store

---
+++ src/store/modules/oauth.js
@@ -0,0 +1,29 @@
+// Importamos nuestra función que obtiene el token
+// @ es un alias para /src
+import * as oauth from '@/api/oauth'
+
+// Creamos el objeto con "Espacio de Nombres"
+export default {
+  namespaced: true,
+  state: {
+    accessToken: null
+  },
+  mutations: {
+    SET_ACCESS_TOKEN (state, payload) {
+      state.accessToken = payload
+    }
+  },
+  actions: {
+    async getToken ({ commit }) {
+      try {
+        const data = await oauth.getToken()
+        commit('SET_ACCESS_TOKEN', data.access_token)
+      } catch (error) {
+        commit('SET_ACCESS_TOKEN', null)
+        console.error('Error OAuth: ', error)
+      } finally {
+        console.log('Done!')
+      }
+    }
+  }
+}
